03cff37dc01c55767cc8b870c3f7eca2c52d9600
Donald J. Fortier II <Technical_13@yahoo.com>
2013-09-12 14:24:46 -0400

    Create Concepts.i18n.magic.php

---
+++ Concepts/Concepts.i18n.magic.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ *
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'concept' => array( 0, 'concept' ),
+);
+
+/** Arabic (العربية) */
+$magicWords['ar'] = array(
+	'concept' => array( 0, 'ودجة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$magicWords['arz'] = array(
+	'concept' => array( 0, 'ودجة', 'concept' ),
+);
+
+/** South Azerbaijani (تورکجه) */
+$magicWords['azb'] = array(
+	'concept' => array( 0, 'آراجلار' ),
+);
+
+/** Chechen (нохчийн) */
+$magicWords['ce'] = array(
+	'concept' => array( 0, 'тlехьажар', 'виджет', 'concept' ),
+);
+
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'concept' => array( 0, 'steuerelement' ),
+);
+
+/** Esperanto (Esperanto) */
+$magicWords['eo'] = array(
+	'concept' => array( 0, 'fenestraĵo', 'fenestrajxo' ),
+);
+
+/** Persian (فارسی) */
+$magicWords['fa'] = array(
+	'concept' => array( 0, 'ابزار' ),
+);
+
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'concept' => array( 0, 'ウィジェット' ),
+);
+
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'concept' => array( 0, '위젯' ),
+);
+
+/** Macedonian (македонски) */
+$magicWords['mk'] = array(
+	'concept' => array( 0, 'посредничкиелемент' ),
+);
+
+/** Malayalam (മലയാളം) */
+$magicWords['ml'] = array(
+	'concept' => array( 0, 'കൂട' ),
+);
+
+/** Marathi (मराठी) */
+$magicWords['mr'] = array(
+	'concept' => array( 0, 'विसाधन', 'concept' ),
+);
+
+/** Oriya (ଓଡ଼ିଆ) */
+$magicWords['or'] = array(
+	'concept' => array( 0, 'ବିଗେଟ' ),
+);
+
+/** Punjabi (ਪੰਜਾਬੀ) */
+$magicWords['pa'] = array(
+	'concept' => array( 0, 'ਵਿਜੈਟ' ),
+);
+
+/** Russian (русский) */
+$magicWords['ru'] = array(
+	'concept' => array( 0, 'виджет' ),
+);
+
+/** Sinhala (සිංහල) */
+$magicWords['si'] = array(
+	'concept' => array( 0, 'ගැජට්', 'concept' ),
+);
+
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+$magicWords['sr-ec'] = array(
+	'concept' => array( 0, 'виџет' ),
+);
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$magicWords['sr-el'] = array(
+	'concept' => array( 0, 'vidžet', 'element' ),
+);
+
+/** Ukrainian (українська) */
+$magicWords['uk'] = array(
+	'concept' => array( 0, 'віджет' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$magicWords['vi'] = array(
+	'concept' => array( 0, 'côngcụđanăng' ),
+);
+
+/** Simplified Chinese (中文（简体）‎) */
+$magicWords['zh-hans'] = array(
+	'concept' => array( 0, '小工具' ),
+);
+
+/** Traditional Chinese (中文（繁體）‎) */
+$magicWords['zh-hant'] = array(
+	'concept' => array( 0, '小工具' ),
+);
